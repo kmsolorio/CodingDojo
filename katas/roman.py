c995cd101f193663e04e7a036ea65cc8f61c5165
Kevin Solorio <kevin@detone8.com>
2013-01-28 23:09:40 -0500

    spacing

--- katas/roman.py
+++ katas/roman.py
@@ -1,8 +1,8 @@
 class RomanNumeral(object):
-  TH = {1 : "M", 2 : "MM", 3 : "MMM"}
-  H = {1 : "C", 2 : "CC", 3 : "CCC", 4 : "CD", 5 : "D", 6 : "DC", 7 : "DCC", 8 : "DCCC", 9 : "CM" }
-  T = {1 : "X", 2 : "XX", 3 : "XXX", 4 : "XL", 5 : "L", 6 : "LX", 7 : "LXX", 8 : "LXXX", 9 : "XC" }
-  O = {1 : "I", 2 : "II", 3 : "III", 4 : "IV", 5 : "V", 6 : "VI", 7 : "VII", 8 : "VIII", 9 : "IX" }
+  TH = { 1 : "M", 2 : "MM", 3 : "MMM" }
+  H =  { 1 : "C", 2 : "CC", 3 : "CCC", 4 : "CD", 5 : "D", 6 : "DC", 7 : "DCC", 8 : "DCCC", 9 : "CM" }
+  T =  { 1 : "X", 2 : "XX", 3 : "XXX", 4 : "XL", 5 : "L", 6 : "LX", 7 : "LXX", 8 : "LXXX", 9 : "XC" }
+  O =  { 1 : "I", 2 : "II", 3 : "III", 4 : "IV", 5 : "V", 6 : "VI", 7 : "VII", 8 : "VIII", 9 : "IX" }
 
   def __init__(self, n):
     self.n = n
