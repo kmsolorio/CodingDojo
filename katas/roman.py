b4389accf39bb7c9275c6e94e266b7b32ec98790
Kevin Solorio <kevin@detone8.com>
2013-02-28 16:18:38 -0500

    dry'd up roman

--- katas/roman.py
+++ katas/roman.py
@@ -1,8 +1,16 @@
 class RomanNumeral(object):
+
   TH = { 1 : "M", 2 : "MM", 3 : "MMM" }
   H =  { 1 : "C", 2 : "CC", 3 : "CCC", 4 : "CD", 5 : "D", 6 : "DC", 7 : "DCC", 8 : "DCCC", 9 : "CM" }
   T =  { 1 : "X", 2 : "XX", 3 : "XXX", 4 : "XL", 5 : "L", 6 : "LX", 7 : "LXX", 8 : "LXXX", 9 : "XC" }
   O =  { 1 : "I", 2 : "II", 3 : "III", 4 : "IV", 5 : "V", 6 : "VI", 7 : "VII", 8 : "VIII", 9 : "IX" }
+
+  Denominations = {
+    1000 : TH,
+    100  : H,
+    10   : T,
+    1    : O
+  }
 
   def __init__(self, n):
     self.n = n
@@ -17,33 +25,22 @@ class RomanNumeral(object):
     return self.numeral
 
   def _set_thousands(self):
-    if self.n >= 1000:
-      t = self.n / 1000
-      self.n -= t * 1000
-      self.numeral += RomanNumeral.TH[t]
-    else:
-      pass
+    self._convert(1000)
 
   def _set_hundreds(self):
-    if self.n >= 100:
-      h = self.n / 100
-      self.n -= h * 100
-      self.numeral += RomanNumeral.H[h]
-    else:
-      pass
+    self._convert(100)
 
   def _set_tens(self):
-    if self.n >= 10:
-      t = self.n / 10
-      self.n -= t * 10
-      self.numeral += RomanNumeral.T[t]
-    else:
-      pass
+    self._convert(10)
 
   def _set_ones(self):
-    if self.n:
-      self.numeral += RomanNumeral.O[self.n]
+    self._convert(1)
+
+  def _convert(self, denomination):
+    if self.n >= denomination:
+      num = self.n / denomination
+      self.n -= num * denomination
+      self.numeral += RomanNumeral.Denominations[denomination][num]
     else:
       pass
-
 
